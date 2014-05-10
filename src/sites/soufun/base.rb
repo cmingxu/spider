80cd6022927bb37c778a7a701b343e236581c475
Kevin.Xu <cming.xu@gmail.com>
2014-05-10 10:00:11 +0800

    soufun base

--- src/sites/soufun/base.rb
+++ src/sites/soufun/base.rb
@@ -8,9 +8,27 @@ module Soufun
       text = Crawler.new.get "http://office.soufun.com/loupan/house/i31/"
 
       doc = Nokogiri::HTML(text)
-      biggest_page = doc.css("div#wrap div.main.mt10 ul.mt10 li.pages.floatr #PageControl1_hlk_last").first["href"].grep(/\d+/)
-      ap biggest_page
+      biggest_page = Integer(doc.css("div#wrap div.main.mt10 ul.mt10 li.pages.floatr #PageControl1_hlk_last").first["href"].scan(/3(\d+)/).flatten.first)
 
+      (1..biggest_page).each do |page|
+        craw_by_page( "http://office.soufun.com/loupan/house/i3#{page}/")
+      end
+    end
+
+
+    def craw_by_page(link)
+      ap link
+      text = Crawler.new.get link
+      doc = Nokogiri::HTML(text)
+      doc.css("div#wrap div.main.mt10 dt p.housetitle a").each do |page|
+        ap page['href']
+        ap page.css("strong").text
+
+        craw_by_office(page)
+      end
+    end
+
+    def craw_by_office(page)
     end
   end
 end
