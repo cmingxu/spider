d6dad8cc0f2ba4f49151de3bb8140e5381f5cb5e
Kevin.Xu <cmxu@kabam.com>
2014-06-19 13:34:54 +0800

    web interface for scrawler

--- src/web.rb
+++ src/web.rb
@@ -1,11 +1,13 @@
-
-
-
 require 'sinatra/base'
-my_app = Sinatra.new do 
+require "daemons"
+
+my_app = Sinatra.new do
   set :root, File.dirname(__FILE__)
   set :public_folder, 'public'
   set :views, Proc.new { File.join(root, "templates") }
-  get('/') { "hi" } 
+  get('/') { haml :index }
+
 end
 my_app.run!
+Daemons.daemonize!
+
